34feaf85dbb4ce3beb171e3d115b1f25bb1e55d5
carlosallberto <dasilvacarlosalberto344@gmail.com>
2025-04-15 16:35:16 -0300

    refatoracao

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -6,6 +6,17 @@ import CredentialsProvider from 'next-auth/providers/credentials'
 import prisma from '@lib/prisma'
 import bcrypt from 'bcryptjs'
 
+declare module 'next-auth' {
+	interface Session {
+		user: {
+			name?: string | null
+			email?: string | null
+			image?: string | null
+			provider?: string | null
+		}
+	}
+}
+
 const handler = NextAuth({
 	providers: [
 		/*================================================*
@@ -42,9 +53,16 @@ const handler = NextAuth({
 					if (user) {
 						let isPasswordCorrect = await bcrypt.compare(credentials?.password!, user.password!)
 						if (isPasswordCorrect) {
-							return user
+							return {
+								id: user.id.toString(),
+								name: user.name,
+								email: user.email,
+								emailVerified: user.emailVerified,
+								image: user.image,
+							}
 						}
 					}
+					return null
 				} catch (err) {
 					throw new Error(String(err))
 				}
@@ -55,7 +73,7 @@ const handler = NextAuth({
 		/*================================================*
 			Registra o usuário no banco de dados caso faça login usando providers pela primeira vez
 		 *================================================*/
-		async signIn({ user, account }: any) {
+		async signIn({ user, account }) {
 			if (account?.provider == 'credentials') {
 				return true
 			}
@@ -65,15 +83,17 @@ const handler = NextAuth({
 			try {
 				let existingUser = await prisma.user.findUnique({ where: { email: user.email! } })
 				if (existingUser) {
-					return false
+					return true
 				}
 
 				await prisma.user.create({ data: { email: user.email, provider: account?.provider } })
+				return true
 			} catch (err) {
 				console.log('Error saving user', err)
+				return false
 			}
 		},
-		jwt({ token, trigger, session, account }: any) {
+		jwt({ token, trigger, session, account }) {
 			if (account) token.provider = account?.provider
 
 			/*================================================*
@@ -82,11 +102,11 @@ const handler = NextAuth({
 			if (trigger === 'update' && session?.name) token.name = session.name
 			return token
 		},
-		async session({ session, token }: any) {
+		async session({ session, token }) {
 			/*================================================*
 				Retorna se o usuário fez login usando formulário ou providers
 			 *================================================*/
-			session.user.provider = token.provider
+			session.user.provider = typeof token.provider === 'string' ? token.provider : null
 			return session
 		},
 	},
